40447a701abf3dc79ea452eb49bee22c5798f952
Dev_Brandon_David_Gonzalez <gonzdeiv123@gmail.com>
2025-05-14 12:25:26 -0500

    Creacion Interfaces de AsignacionRutas, implementacion plantillas service y useCase

--- NexLogix/backend/app/Models/AsignacionRutas.php
+++ NexLogix/backend/app/Models/AsignacionRutas.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 
+//RELACION N:1 con vehiculos y rutas
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
---
+++ NexLogix/backend/app/Services/AsignacionRutas/AsignacionRutasService.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Interfaces\AsignacionRutas\IAsignacionRutasService;
+
+class AsignacionRutasService implements IAsignacionRutasService
+{
+    public function getAllCiudades()
+    {
+        
+    }
+    public function getCiudadById(int $id): array
+    {
+
+    }
+    public function createCiudad(array $data): array
+    {
+
+    }
+    public function updateCiudad(int $id, array $data)
+    {
+
+    }
+    public function updateSpecificSectionCiudad(int $id, array $data): array
+    {
+
+    }
+    public function deleteCiudad(int $id): array
+    {
+
+    }
+}
