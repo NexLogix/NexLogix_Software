98c4ac92fe1844f22c219c8997acc8ed73266e84
Dev_Brandon_David_Gonzalez <gonzdeiv123@gmail.com>
2025-05-15 15:49:36 -0500

    Eliminacion de GestionAsignacionRutas en GestionVehiculos, solucion de conflictos

--- NexLogix/backend/app/Models/Interfaces/AsignacionRutas/IAsignacionRutasUseCase.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models\Interfaces\AsignacionRutas;
-
-interface IAsignacionRutasUseCase
-{
-    public function handleCreateCiudad(array $data): array;
-    public function handleUpdateCiudad(int $id, array $data);
-}
