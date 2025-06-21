c349b7cfc782d4210f1ebb4025a50faf56d0a4d5
Esteban <rakliox10000@gmail.com>
2025-06-21 16:40:06 -0500

    Se corrige el ''NavBar'' con los botones definidos

--- NexLogix/frontend/src/Views/profiles/ManagerProfile/ManagerProfile.tsx
+++ NexLogix/frontend/src/Views/profiles/ManagerProfile/ManagerProfile.tsx
@@ -1,139 +1,96 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import FooterGeneralManager from "../../componets/Footers/FooterManager";
 import NavbarGeneral from "../../componets/NavBars/NavbarGeneral";
 import '../../../Views/Styles/Profiles/ProfilesGeneralStyle.css';
 
 const ManagerProfile = () => {
- return (
-  /// aqui tienen que poner lo demas....
-        <>
-        <NavbarGeneral/>
-        {/* Contenedor principal */} 
-        <div className="container-fluid">
-          <div className="row">
+  return (
+    /// aqui tienen que poner lo demas....
+    <>
+      <NavbarGeneral />
+      {/* Contenedor principal */}
+      <div className="container-fluid">
+        <div className="row">
 
-            {/* Sidebar: barra lateral de navegación */}
-            <nav id="sidebar" className=" col-lg-2 d-md-block sidebar">
-              <div className="position-sticky">
-                <ul className="nav flex-column">
+          {/* Sidebar: barra lateral de navegación */}
+          <nav id="sidebar" className=" col-lg-2 d-md-block sidebar">
+            <div className="position-sticky">
+              <ul className="nav flex-column">
 
-                        {/* Opción Inicio */}
-                        <Link className="nav-link active" to="/manager">INICIO MANAGER</Link>
+                {/* Opción Inicio */}
+                <Link className="nav-link active" to="/manager">INICIO MANAGER</Link>
 
-                        {/* Opción Gestión Áreas con submenú */}
-                        <li className="nav-item">
-                          <Link className="nav-link" data-bs-toggle="collapse" to="#AreasSubmenu" role="button" aria-expanded="false" aria-controls="AreasSubmenu">
-                            Gestion Usuarios
-                          </Link>
-                          <div className="collapse" id="AreasSubmenu">
-                            <ul className="nav flex-column">
-                              <Link className="nav-link" to="/manager/verAreas">Areas</Link>
-                              <Link className="nav-link" to="/manager/crearArea">Puestos</Link>
-                              <Link className="nav-link" to="/manager/editarArea">Reportes</Link>
-                              <Link className="nav-link" to="/manager/eliminarArea">Usuarios</Link>
-                            </ul>
-                          </div>
-                        </li>
+                {/* Opción Gestión Áreas con submenú */}
+                <li className="nav-item">
+                  <Link className="nav-link" data-bs-toggle="collapse" to="#AreasSubmenu" role="button" aria-expanded="false" aria-controls="AreasSubmenu">
+                    Adminstración
+                  </Link>
+                  <div className="collapse" id="AreasSubmenu">
+                    <ul className="nav flex-column">
+                      <Link className="nav-link" to="/manager/verAreas">Areas</Link>
+                      <Link className="nav-link" to="/manager/crearArea">Puestos</Link>
+                      <Link className="nav-link" to="/manager/editarArea">Reportes</Link>
+                      <Link className="nav-link" to="/manager/editarArea">Roles</Link>
+                      <Link className="nav-link" to="/manager/eliminarArea">Usuarios</Link>
+                    </ul>
+                  </div>
+                </li>
 
-                      {/* Opción Auditorías con submenú */}
-                        <li className="nav-item">
-                          <Link className="nav-link" data-bs-toggle="collapse" to="#AuditoriasSubmenu" role="button" aria-expanded="false" aria-controls="AuditoriasSubmenu">
-                            Gestion Auditorias
-                          </Link>
-                          <div className="collapse" id="AuditoriasSubmenu">
-                            <ul className="nav flex-column">
-                              <Link className="nav-link" to="/manager/verAuditorias">Lista de auditorías</Link>
-                              <Link className="nav-link" to="/manager/editarAuditorias">Editar auditorías</Link>
-                              <Link className="nav-link" to="/manager/eliminarAuditorias">Eliminar auditorías</Link>
-                            </ul>
-                          </div>
-                        </li>
+                {/* Opción Auditorías con submenú */}
+                <li className="nav-item">
+                  <Link className="nav-link" data-bs-toggle="collapse" to="#AuditoriasSubmenu" role="button" aria-expanded="false" aria-controls="AuditoriasSubmenu">
+                    Gestion Auditorias
+                  </Link>
+                  <div className="collapse" id="AuditoriasSubmenu">
+                    <ul className="nav flex-column">
+                      <Link className="nav-link" to="/manager/verAuditorias">Lista de auditorías</Link>
+                      <Link className="nav-link" to="/manager/editarAuditorias">Editar auditorías</Link>
+                      <Link className="nav-link" to="/manager/eliminarAuditorias">Eliminar auditorías</Link>
+                    </ul>
+                  </div>
+                </li>
+
+                {/* Opción Ciudades con submenú */}
+                <li className="nav-item">
+                  <Link className="nav-link" data-bs-toggle="collapse" to="#CiudadesSubmenu" role="button" aria-expanded="false" aria-controls="CiudadesSubmenu">
+                    Gestion Ciudades
+                  </Link>
+                  <div className="collapse" id="CiudadesSubmenu">
+                    <ul className="nav flex-column">
+                      <Link className="nav-link" to="/manager/verCiudades">Lista de ciudades</Link>
+                      <Link className="nav-link" to="/manager/crearCiudad">Crear ciudad</Link>
+                      <Link className="nav-link" to="/manager/editarCiudad">Editar ciudad</Link>
+                      <Link className="nav-link" to="/manager/eliminarCiudad">Eliminar ciudad</Link>
+                    </ul>
+                  </div>
+                </li>
+
+                {/* Opción Envíos con submenú */}
+                <li className="nav-item position-relative ">
+                  <Link className="nav-link" data-bs-toggle="collapse" to="#enviosSubmenu" role="button" aria-expanded="false" aria-controls="enviosSubmenu">Gestion Envíos</Link>
+                  <div className="collapse" id="enviosSubmenu">
+                    <ul className="nav flex-column">
+                      <Link className="nav-link" to="/manager/verEnvios">Ver Envíos</Link>
+                      <Link className="nav-link" to="/manager/crearEnvios">Crear Envío</Link>
+                      <Link className="nav-link" to="/manager/editarEnvios">Editar Envío</Link>
+                      <Link className="nav-link" to="/manager/eliminarEnvio">Eliminar Envío</Link>
+                    </ul>
+                  </div>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link" data-bs-toggle="collapse" to="#VehiculosSubmenu" role="button" aria-expanded="false" aria-controls="VehiculosSubmenu">Gestión Logística</Link>
+                  <div className="collapse" id="VehiculosSubmenu">
+                    <ul className="nav flex-column">
+                      <Link className="nav-link" to="/manager/verListaVehiculos">Lista de vehículos</Link>
+                      <Link className="nav-link" to="/manager/verConductores">Conductores</Link>
+                    </ul>
+                  </div>
+                </li>
 
 
-                      {/* Opción Puestos con submenú */}
-                        <li className="nav-item">
-                          <Link className="nav-link" data-bs-toggle="collapse" to="#PuestosSubmenu" role="button" aria-expanded="false" aria-controls="PuestosSubmenu">
-                            Gestion Puestos
-                          </Link>
-                          <div className="collapse" id="PuestosSubmenu">
-                            <ul className="nav flex-column">
-                              <Link className="nav-link" to="/manager/verPuestos">Lista de puestos</Link>
-                              <Link className="nav-link" to="/manager/crearPuesto">Crear puesto</Link>
-                              <Link className="nav-link" to="/manager/editarPuesto">Editar puesto</Link>
-                              <Link className="nav-link" to="/manager/eliminarPuesto">Eliminar puesto</Link>
-                            </ul>
-                          </div>
-                        </li>
-
-                      {/* Opción Ciudades con submenú */}
-                        <li className="nav-item">
-                          <Link className="nav-link" data-bs-toggle="collapse" to="#CiudadesSubmenu" role="button" aria-expanded="false" aria-controls="CiudadesSubmenu">
-                            Gestion Ciudades
-                          </Link>
-                          <div className="collapse" id="CiudadesSubmenu">
-                            <ul className="nav flex-column">
-                              <Link className="nav-link" to="/manager/verCiudades">Lista de ciudades</Link>
-                              <Link className="nav-link" to="/manager/crearCiudad">Crear ciudad</Link>
-                              <Link className="nav-link" to="/manager/editarCiudad">Editar ciudad</Link>
-                              <Link className="nav-link" to="/manager/eliminarCiudad">Eliminar ciudad</Link>
-                            </ul>
-                          </div>
-                        </li>
-
-                        {/* Opción Envíos con submenú */}
-                        <li className="nav-item position-relative ">
-                            <Link className="nav-link" data-bs-toggle="collapse" to="#enviosSubmenu" role="button" aria-expanded="false" aria-controls="enviosSubmenu">Gestion Envíos</Link>
-                            <div className="collapse" id="enviosSubmenu">
-                                  <ul className="nav flex-column">
-                                      <Link className="nav-link" to="/manager/verEnvios">Ver Envíos</Link>
-                                      <Link className="nav-link" to="/manager/crearEnvios">Crear Envío</Link>
-                                      <Link className="nav-link" to="/manager/editarEnvios">Editar Envío</Link>
-                                      <Link className="nav-link" to="/manager/eliminarEnvio">Eliminar Envío</Link>
-                                  </ul>
-                            </div>
-                        </li>
-
-                        {/* Opción Empleados con submenú */}
-                        <li className="nav-item">
-                          <Link className="nav-link" data-bs-toggle="collapse" to="#EmpleadosSubmenu" role="button" aria-expanded="false" aria-controls="EmpleadosSubmenu">Gestion Empleados</Link>
-                              <div className="collapse" id="EmpleadosSubmenu">
-                                    <ul className="nav flex-column">
-                                      <Link className="nav-link" to="crearUsuarios">Crear usuarios</Link>
-                                      <Link className="nav-link" to="/manager/verUsuarios">Lista general</Link>
-                                      <Link className="nav-link" to="/manager/editarUsuarios">Editar usuario</Link>
-                                      <Link className="nav-link" to="/manager/desactivarUsuario">Desactivar Usuario</Link>
-                                      <Link className="nav-link" to="/manager/eliminarUsuarios">Eliminar usuario</Link>
-                                    </ul>
-                              </div>
-                        </li>
-
-                        {/* Opción Rutas con submenú */}
-                        <li className="nav-item">
-                              <Link className="nav-link" data-bs-toggle="collapse" to="#RutasSubmenu"  role="button" aria-expanded="false"  aria-controls="RutasSubmenu" >Gestion Rutas</Link>
-                              <div className="collapse" id="RutasSubmenu">
-                                    <ul className="nav flex-column">
-                                      <Link className="nav-link" to="/manager/verListaRutas">Ver lista de rutas</Link>
-                                      <Link className="nav-link" to="/manager/editarRutas">Editar rutas</Link>
-                                      <Link className="nav-link" to="/manager/eliminarRutas">Eliminar rutas</Link>
-                                    </ul>
-                              </div>
-                        </li>
-
-                        
-                        
-                          <li className="nav-item">
-                            <Link className="nav-link" data-bs-toggle="collapse" to="#VehiculosSubmenu" role="button" aria-expanded="false" aria-controls="VehiculosSubmenu">Gestion Vehículos</Link>
-                            <div className="collapse" id="VehiculosSubmenu">
-                                <ul className="nav flex-column">
-                                  <Link className="nav-link" to="/manager/verListaVehiculos">Lista de vehículos</Link>
-                                  <Link className="nav-link" to="/manager/verConductores">Conductores</Link>
-                                </ul>
-                            </div>
-                        </li>
-
-                        
-                        {/* 
+                {/* 
                         // AREA DE BOTONES SOPORTE TECNICO
                           <li className="nav-item">
                             <Link className="nav-link" data-bs-toggle="collapse" to="#SoporteTecnicoSubMenu" role="button" aria-expanded="false" aria-controls="SoporteTecnicoSubMenu">Soporte Técnico</Link>
@@ -146,32 +103,29 @@ const ManagerProfile = () => {
                                 </div>
                           </li> 
                         */}
-                        
 
-                        {/* Opción Reportes */}
-                        <li className="nav-item">
-                            <Link className="nav-link" data-bs-toggle="collapse" to="#ReportesSubMenu" role="button" aria-expanded="false" aria-controls="ReportesSubMenu">Gestion Reportes</Link>
-                                <div className="collapse" id="ReportesSubMenu"> 
-                                      <ul className="nav flex-column">
-                                        <Link className="nav-link" to="/manager/verReportes">Reportes</Link>
-                                      </ul>
-                                </div>
-                        </li>
 
-                </ul>
-              </div>
-            </nav>
+                {/* Opción Reportes */}
+                <li className="nav-item">
+                    <ul className="nav flex-column">
+                      <Link className="nav-link" to="/manager/verReportes">Reportes</Link>
+                    </ul>
+                </li>
 
-            {/* Área principal (Main) */}
-              <main>
-                <Outlet />  {/* Aquí se renderiza el contenido según la ruta */}
-              </main>
-          </div>
+              </ul>
+            </div>
+          </nav>
+
+          {/* Área principal (Main) */}
+          <main>
+            <Outlet />  {/* Aquí se renderiza el contenido según la ruta */}
+          </main>
         </div>
+      </div>
 
-        {/* Pie de página */}
-        <FooterGeneralManager />
-      </>
+      {/* Pie de página */}
+      <FooterGeneralManager />
+    </>
   );
 };
 
